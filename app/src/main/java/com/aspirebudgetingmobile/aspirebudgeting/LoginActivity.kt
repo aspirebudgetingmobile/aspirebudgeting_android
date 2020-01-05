eb48b9c48a660352d3355c9e3dfe2b2add790b21
Abhinav Gupta <abhi.951990@gmail.com>
2020-01-05 00:55:56 -0800

    Adding background and youtube player

--- app/src/main/java/com/aspirebudgetingmobile/aspirebudgeting/LoginActivity.kt
+++ app/src/main/java/com/aspirebudgetingmobile/aspirebudgeting/LoginActivity.kt
@@ -2,20 +2,22 @@ package com.aspirebudgetingmobile.aspirebudgeting
 
 //import android.R
 
-import android.R.attr
+//import android.R
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
+//import sun.jvm.hotspot.utilities.IntArray
 
 
 class LoginActivity : AppCompatActivity(), View.OnClickListener {
@@ -28,16 +30,18 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
         private const val RC_SIGN_IN = 1
     }
 
+    private lateinit var youTubePlayerView: YouTubePlayerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 //        setSupportActionBar(toolbar)
 //        findViewById(R.id.sign_in_button).setOnClickListener(this);
-        findViewById<com.google.android.gms.common.SignInButton>(R.id.sign_in_button).setOnClickListener(
-            this
-        )
+        findViewById<Button>(R.id.sign_in_button)
+            .setOnClickListener(this)
 
-
+        youTubePlayerView = findViewById(R.id.youtube_player_view)
+        lifecycle.addObserver(youTubePlayerView)
     }
 
     override fun onClick(v: View?) {
@@ -46,6 +50,10 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+//        youTubePlayerView.release()
+    }
     private fun signIn() {
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestEmail()
@@ -60,9 +68,12 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == RC_SIGN_IN) {
-            val task =
-                GoogleSignIn.getSignedInAccountFromIntent(data)
-            handleSignInResult(task)
+            if(resultCode == Activity.RESULT_OK){
+                val task =
+                    GoogleSignIn.getSignedInAccountFromIntent(data)
+                handleSignInResult(task)
+            }
+
         }
     }
 
@@ -71,7 +82,8 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
             val account: GoogleSignInAccount? = completedTask?.result
             // Signed in successfully, show authenticated UI.
 //            updateUI(account)
-        } catch (e: ApiException) { // The ApiException status code indicates the detailed failure reason.
+        } catch (e: ApiException) {
+            // The ApiException status code indicates the detailed failure reason.
 // Please refer to the GoogleSignInStatusCodes class reference for more information.
 //            Log.w(FragmentActivity.TAG, "signInResult:failed code=" + e.statusCode)
 //            updateUI(null)
